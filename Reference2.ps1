9ae51c275f2dd61dba4bdbd34caaffad57a0c3b8
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-02-18 23:55:37 +0800

    Update and rename reference_Logic_Files to Reference2.ps1

--- Reference2.ps1
+++ Reference2.ps1
@@ -1,5 +1,4 @@
 #----- Array Appraoch#1   
-
 $updateFeatures = @(   
   'UpdateServices-WidDB',  
   'UpdateServices-Services',  
@@ -11,41 +10,25 @@ $updateFeatures = @(
 Write-Output $updateFeatures  
 Install-WindowsFeature -Name $updateFeatures  
 
-
- #----- Array Appraoch#2  
-
+#----- Array Appraoch#2  
 $Directory= 'D:SRSReportKeys', 'F:MSSQL', 'F:MSSQLTempDB', 'F:MSSQLUserDB', 'G:MSSQL', 'G:MSSQLUserDBLOG', 'G:MSSQLTempDBLogs', 'G:MSSQLBackup'   
 
- 
 
 $Directory|foreach-object{   
-
 New-Item $_ -Type Directory 
-
 $NewAcl = Get-Acl -Path $_  
-
 Set-Acl -Path $_ -AclObject $NewAcl   
-
 }  
-
- 
-
-  
 
 ################ Date ################  
 
 $StartTime=Get-Date -Year 2019 -Month 11 -Day 1 -Hour 00 -Minute 00  
-
 $EndTime=Get-Date -Year 2020 -Month 11 -Day 1 -Hour 00 -Minute 00  
-
 $hostname="vwts19bk-easd12"  
- 
 
 #----- Object Handling -----#  
-
 Compare-Object -ReferenceObject $(Get-Content C:\test\testfile1.txt) -DifferenceObject $(Get-Content C:\test\testfile2.txt)                          
 
-  
 
 #----- File Handling -----#        
 Measure-Command {Get-ChildItem -Path C:\Windows\*.txt -Recurse}                  
@@ -54,7 +37,6 @@ Get-ChildItem -path c:\tmp–Recurse| Measure-Object -Property length -Minimum -
 #----- Read the content of file  
 Get-Content -Path $file| Format-Table -AutoSize  
  
-
 #----- create a local file for schedule task  
 
 New-Item $file -force  
