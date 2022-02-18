51b556f064455372b1c8056f52af7ebef3f6e18f
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-02-18 23:59:09 +0800

    Update Troubleshooting.ps1

--- Troubleshooting.ps1
+++ Troubleshooting.ps1
@@ -70,5 +70,56 @@ WHERE registry_key LIKE '%IPALL' AND value_name in ('TcpPort','TcpDynamicPorts')
 
 
 
+$File =  "c:\tmp\$env:COMPUTERNAME.txt"
+New-Item $file -force
+Add-Content $file '$ServerName="' -NoNewline
+Add-Content $file "$env:COMPUTERNAME" -NoNewline
+Add-Content $file '"'
+
+#----- Capture system information.
+cd c:\tmp\
+GPresult /H GPresult.html
+Get-hotfix |Out-File -FilePath  $file -append -Encoding ascii
+systeminfo |Out-File -FilePath  $file -append -Encoding ascii
+
+
+#----- Enquiry local Password Policy
+Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters\" |Out-File -FilePath  $file -append -Encoding ascii
+
+#----- Enquiry Local Secuirty policy
+secedit /export /cfg c:secpolori.inf
+type c:secpolori.inf |Out-File -FilePath  $file -append -Encoding ascii
+
+
+#-----Enquiry Permission of Folder
+(get-acl c:\tmp).access | ft IdentityReference,FileSystemRights,AccessControlType,IsInherited,InheritanceFlags -auto |Out-File -FilePath  $file -append -Encoding ascii
+
+#----- Enquiry Permission of Registry Key
+(get-acl "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server 2016 Redist").access | ft IdentityReference,FileSystemRights,AccessControlType,IsInherited,InheritanceFlags -auto  |Out-File -FilePath  $file -append -Encoding ascii
+
+#----- Enquiry Volume information
+get-volume  |Out-File -FilePath  $file -append -Encoding ascii
+
+
+
+#----- Enquiry Auto start Windows service.
+get-service | select DisplayName, ServiceName, StartType,Status  |findstr Automatic  |Out-File -FilePath  $file -append -Encoding ascii
+
+#----- Enquiry particular Windows service details.
+get-service -displayname "SQL*"  | select DisplayName, ServiceName, StartType,Status  |Out-File -FilePath  $file -append -Encoding ascii
+
+#----- Enquiry particular Windows service details.
+Get-WMIObject -Class Win32_Service -Filter  "Name='MSSQLSERVER'" |   Select-Object Name, DisplayName, StartMode, Status, StartName  |Out-File -FilePath  $file -append -Encoding ascii
+
+
+Get-NetAdapter  |Out-File -FilePath  $file -append -Encoding ascii
+
+
+
+Get-CimInstance Win32_OperatingSystem | Select-Object  Caption, InstallDate, ServicePackMajorVersion, OSArchitecture, BootDevice,  BuildNumber, CSName | FL
+
+
+
+
 
 
