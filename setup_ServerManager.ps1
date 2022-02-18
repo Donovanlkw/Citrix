f1d30a5e6d6dea42d87c2199b3f709e3e7d5eb0b
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-02-18 23:37:02 +0800

    Update and rename setup_ServerManager to setup_ServerManager.ps1

--- setup_ServerManager.ps1
+++ setup_ServerManager.ps1
@@ -1,97 +1,50 @@
 #----- Creat a deault template for ServerManager -----# 
 
 Get-process ServerManager | stop-process -force 
-
 del "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\ServerManager\ServerList.xml" 
-
 start-process -filepath $env:SystemRoot\System32\ServerManager.exe -WindowStyle Maximized 
-
 timeout 10 
-
 Get-process ServerManager | % { $_.CloseMainWindow() } 
-
-  
-
-  
 
 #----- list out all RDS Server and add to server manager via For loop -----# 
-
-  
-
 Import-Module ServerManager 
-
 $RDServer = Get-rdserver |select-object server 
-
 $RDServer |ForEach-Object { 
-
 $file = get-item "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\ServerManager\ServerList.xml" 
 
 copy-item -path $file -destination $file-backup -force 
-
 $xml = [xml] (get-content $file ) 
-
 $newserver = @($xml.ServerList.ServerInfo)[0].clone() 
-
 $newserver.name = $_.server 
 
-  
-
 $newserver.lastUpdateTime = "0001-01-01T00:00:00" 
-
 $newserver.status = "2" 
-
 $xml.ServerList.AppendChild($newserver) 
-
 $xml.Save($file.FullName) 
-
 } 
 
-  
-
 start-process -filepath $env:SystemRoot\System32\ServerManager.exe -WindowStyle Maximized 
-
- 
-
- 
 
 #----- Add server in Server Manager 
-
 Get-process ServerManager | stop-process -force 
-
 del "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\ServerManager\ServerList.xml" 
-
 start-process -filepath $env:SystemRoot\System32\ServerManager.exe -WindowStyle Maximized 
-
 timeout 10 
 
-Get-process ServerManager | % { $_.CloseMainWindow() } 
-
-  
+Get-process ServerManager | % { $_.CloseMainWindow() }  
 
 Import-Module ServerManager 
-
 $RDServer = Get-rdserver |select-object server 
-
 $RDServer |ForEach-Object { 
-
 $file = get-item "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\ServerManager\ServerList.xml" 
-
 copy-item -path $file -destination $file-backup -force 
-
-$xml = [xml] (get-content $file ) 
-
+$xml = [xml] (get-content $file )
 $newserver = @($xml.ServerList.ServerInfo)[0].clone() 
-
 $newserver.name = $_.server 
-
 $newserver.lastUpdateTime = "0001-01-01T00:00:00" 
-
 $newserver.status = "2" 
-
 $xml.ServerList.AppendChild($newserver) 
-
 $xml.Save($file.FullName) 
-
 } 
 
 start-process -filepath $env:SystemRoot\System32\ServerManager.exe -WindowStyle Maximized
