f338918f11e9d3b57a764e58926abc32e9ded4cc
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-02-18 23:49:20 +0800

    Update and rename reference_variable.ps1 to Reference_variable.ps1

--- Reference_variable.ps1
+++ Reference_variable.ps1
@@ -50,77 +50,41 @@ Register-ScheduledTask -TaskName $task -user $user -password $pwd -Action $act -
 ### create a local file for schedule task   
 
 New-Item $file -force  
-
 Add-content $file 'ServerManager'  
-
 Add-content $file 'Start-Sleep -s 300'  
-
 Add-Content $file '$BrokerServer="' -NoNewline  
-
 Add-Content $file "$BrokerServer" -NoNewline  
-
 Add-Content $file '"'  
-
 Add-Content $file '$VMSSASH = Get-rdserver |select-object Server |where server -like ' -NoNewline   
-
 Add-Content $file "$vmssName" -NoNewline  
-
 Add-Content $file '*'   
-
 Add-Content $file '  
 
-  
-
 $VMSSASH |ForEach-Object {   
-
 Remove-RDSessionHost  -SessionHost $_.server -ConnectionBroker $BrokerServer  -force   
-
 Remove-RDServer       -Server $_.server -Role "RDS-RD-SERVER" -ConnectionBroker $BrokerServer -force  
-
 }  
 
-'   
-
- 
-
- 
-
 #----- Reset security configuration   
-
 Add-LocalGroupMember -Group "Administrators" -Member $user  
-
 Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\" -Name "MinEncryptionLevel"   
-
 set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\" -Name "DisableDomainCreds" -value "0"  
-
 set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -value "0"  
-
-    
-
  
 
 #----- Reset security policy configuration   
-
 secedit /export /cfg c:\temp\secpolori.inf  
-
 (gc c:\temp\secpolori.inf).replace("SeServiceLogonRight =", "SeServiceLogonRight = *S-1-5-80-0,") | Out-File c:\temp\secpolnew.inf  
-
 (gc c:\temp\secpolori.inf).replace("SeServiceLogonRight =", "SeServiceLogonRight = *S-1-5-80-1184457765-4068085190-3456807688-2200952327-3769537534,") | Out-File c:\temp\secpolfinal.inf  
-
 secedit /configure /db C:\Windows\security\local.sdb /areas USER_RIGHTS /cfg  c:\temp\secpolnew.inf  
-
-  
+ 
 
 #----- Restore security configuration  
-
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\"  -Name "MinEncryptionLevel" -Value "3"  -PropertyType "DWORD"  
-
 set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\" -Name "DisableDomainCreds" -value "1"  
-
 set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -value "1"  
 
   
 
 #----- Restore security policy configuration  
-
 secedit /configure /db C:\Windows\security\local.sdb /areas USER_RIGHTS /cfg  c:\temp\secpolfinal.inf   
