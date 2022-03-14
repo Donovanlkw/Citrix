8c9eaba24ef75c49d0bc5f75c59208e8ce16b4fc
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-03-14 10:15:39 +0800

    Update BAU.healthcheck.ps1

--- BAU.healthcheck.ps1
+++ BAU.healthcheck.ps1
@@ -1,14 +1,12 @@
 ###--- check the Xenapp status ---###
 
+$ComputerName = "serverA`r`nServerB`r`nServerC"
+$computerName |out-file serverlist.txt
 
-$computer = "DDC"
-Invoke-Command -Computer $computer -ScriptBlock {
-	Get-Brokermachine -SessionSupport MultiSession
- } |	select CatalogName, DNSName, ImageOutOfDate, InMaintenanceMode, IsAssigned, LoadIndex, SessionCount |Format-table -AutoSize
+$parameters = @{
+  ComputerName = Get-Content serverlist.txt
+  ScriptBlock = {Get-Brokermachine -SessionSupport MultiSession}
+}
+Invoke-Command @parameters |select CatalogName, DNSName, ImageOutOfDate, InMaintenanceMode, IsAssigned, LoadIndex, SessionCount |Format-table -AutoSize
 
-
-Get-Brokermachine -SessionSupport MultiSession |select CatalogName, DNSName, ImageOutOfDate, InMaintenanceMode, IsAssigned, LoadIndex, SessionCount |Format-table -AutoSize
-
-
-###--- check the server status ---###
-
+###--- check the Xenapp status ---###
