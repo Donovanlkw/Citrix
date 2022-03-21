0c96a87e2b6b78dc9b0eb3c10825b3756db7dff1
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-03-21 12:22:20 +0800

    Delete ACL.ps1

--- ACL.ps1
+++
@@ -1,9 +0,0 @@
-#Get the VM Inventory from Citrix Studio
-
-$computer = "DDC"
-Invoke-Command -Computer $computer -ScriptBlock {
-	Get-BrokerDesktop   -MaxRecordCount 10000 
- } |	Select-Object MachineName, @{l="AssociatedUserNames";e={$_.AssociatedUserNames -join ","}} |Export-Csv $computer
- 
-# Merge all the files
-Get-ChildItem -include azaw* -Recurse |get-content|Out-File StudioInventory -NoClobber
