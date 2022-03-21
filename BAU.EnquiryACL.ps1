4b898447bfb5a3aa06f61f95eb9bfca906bb61a4
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-03-21 12:21:58 +0800

    Update and rename BAU.EnquiryACL to BAU.EnquiryACL.ps1

--- BAU.EnquiryACL.ps1
+++ BAU.EnquiryACL.ps1
@@ -24,6 +24,8 @@ Get-BrokerApplication -AllAssociatedDesktopGroupUUID $_.UUID.guid -filter {Enabl
 Invoke-Command @parameters | Select-Object name, AllAssociatedDesktopGroupUUIDs, UserFilterEnabled, @{l="AssociatedUserNames";e={$_.AssociatedUserNames -join ","}} |export-csv EnabledApplication.csv
 
 
+
+
 ####################################################################
 
 #Get the ACL for Destkop
@@ -43,4 +45,12 @@ Get-BrokerApplication -MaxRecordCount 10000 | Select-Object name, UserFilterEnab
 
 ####################################################################
 
-#Get the SecureICA configuration 
+#Get the VM Inventory from Citrix Studio
+
+$computer = "DDC"
+Invoke-Command -Computer $computer -ScriptBlock {
+	Get-BrokerDesktop   -MaxRecordCount 10000 
+ } |	Select-Object MachineName, @{l="AssociatedUserNames";e={$_.AssociatedUserNames -join ","}} |Export-Csv $computer
+ 
+# Merge all the files
+Get-ChildItem -include azaw* -Recurse |get-content|Out-File StudioInventory -NoClobber
