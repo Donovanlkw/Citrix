462076f131d93dcc7425671be7de2d55adad7973
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-03-22 23:39:52 +0800

    Create New.SF.ps1

---
+++ New.SF.ps1
@@ -0,0 +1,9 @@
+https://support.citrix.com/article/CTX206009
+
+https://developer-docs.citrix.com/projects/storefront-powershell-sdk/en/latest/Get-STFStoreService/
+
+https://dennisspan.com/translating-the-citrix-storefront-console-to-powershell/
+
+https://github.com/citrix/storefront-powershell-sdk-old/blob/master/docs/storefront-services-authentication-sdk.md
+
+https://www.mycugc.org/blogs/chris-jeucken/2021/03/02/unattended-storefront-install-and-config
