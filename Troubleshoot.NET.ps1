c8cea0992da55cc20a525177bcee5437411518c5
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-05-13 23:04:04 +0800

    Update Troubleshoot.NET.ps1

--- Troubleshoot.NET.ps1
+++ Troubleshoot.NET.ps1
@@ -2,3 +2,13 @@
 
 
 measure-command {test-netconnection @IP -port 443 } |% TotalMilliseco
+
+
+
+$FQDN1="asiacitrix.manulife.com"
+$FQDN2="citrix.manulife.com"
+
+$DNSIP|foreach-object{   
+#Resolve-DnsName -Name $FQDN1 -Server $_
+Resolve-DnsName -Name $FQDN2 -Server $_
+} 
