e6b4ebe0b037f67c3aff6bbaf8503a3d6642f4f5
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-11-29 16:43:59 +0800

    Update MCS.snapshot.ps1

--- MCS.snapshot.ps1
+++ MCS.snapshot.ps1
@@ -1,28 +1,34 @@
-### --- Perform Patching healthcheck
+### --- Perform Patching healthcheck 
 
-$ComputerName="AZAWVCTXVDAD01`
+
+### --- Graceful shutdown via Citrix Studio.
+
+
+$ComputerName="AZAWVCTXVDAD01"
 
 $ComputerName = Get-Content 'Non-Prod_Master.txt'
 #$ComputerName = Get-Content 'SEA_Master.txt'
 #$ComputerName = Get-Content 'EAS_Master.txt'
 #$ComputerName = Get-Content 'JPE_Master.txt'
 #$ComputerName = Get-Content 'EnI_Master.txt'
+$ComputerName| Foreach-object {
+New-BrokerHostingPowerAction -Action Shutdown -MachineName $_
+}
 
+
+
+
+### --- Taking Snapshot in Azure ---###
 $resourceGroupName = 'mfc-rg-gis-ctx-eas'
 $location = 'eastasia' 
 Select-AzSubscription MFC-Asia-DevQA_Internal-S1 
 $dateStr = Get-Date -Format "yyyyMMdd"
 
-###--- Reboot and Shutdown the server ---###
 $ComputerName| Foreach-object {
-#Restart-AzVM -ResourceGroupName $resourceGroupName -Name $_.
-Stop-AzVM -ResourceGroupName $resourceGroupName -Name $_ -
-}
-
-### --- Taking Snapshot in Azure ---###
-$ComputerName| Foreach-object {
-$SnapshotName= $_.+"-"+$dateStr+".snapshot"
-$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $MasterServer
+$SnapshotName= $_+"-"+$dateStr+".snapshot"
+$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $_
 $snapshotconfig =  New-AzSnapshotConfig -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy
 $Snapshot = New-AzSnapshot -Snapshot $snapshotconfig -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName 
 }
+
+
