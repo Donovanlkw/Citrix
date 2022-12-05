430c4a2a576dd3e17e3589af50ab276f8ce7d36f
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-12-05 12:04:42 +0800

    Update Ref.Array.ps1

--- Ref.Array.ps1
+++ Ref.Array.ps1
@@ -1,3 +1,23 @@
+$array = @()
+$array += $obj
+
+
+#----- Array Appraoch#1 
+$updateFeatures = @(
+  'UpdateServices-WidDB',
+  'UpdateServices-Services',
+  'UpdateServices-RSAT',
+)
+Write-Output $updateFeatures
+#Install-WindowsFeature -Name $updateFeatures
+
+#----- Array Appraoch#2
+$Directory= 'D:SRSReportKeys', 'F:MSSQL', 'F:MSSQLTempDB', 'F:MSSQLUserDB', 'G:MSSQL', 'G:MSSQLUserDBLOG', 'G:MSSQLTempDBLogs', 'G:MSSQLBackup'
+$Directory|foreach-object{
+New-Item $_ -Type Directory
+$NewAcl = Get-Acl -Path $_
+Set-Acl -Path $_ -AclObject $NewAcl
+}
 
 ### ---  combine two Array variable into a Table --- ###
 $report =@()
@@ -5,8 +25,6 @@ $first = @(1,2,3,4,5)
 $second = @(6, 7,8,9,10)
 
 $report= for($i= 0; $i -lt $first.count; $i++) {  
-#write-Verbose "$first[$i],$Second[$i])"
-
 [PSCustomObject]@{
 first= $first[$i]
 Second= $Second[$i]
