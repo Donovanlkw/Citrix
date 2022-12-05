911ee096b42bba03742ce261467f203f350b8c54
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-12-05 14:29:35 +0800

    Update Healthchck.MCS.ps1

--- Healthchck.MCS.ps1
+++ Healthchck.MCS.ps1
@@ -1,3 +1,22 @@
+<### --- Healthcheck v2  --- ###>
+$DDC |ForEach{
+$result=Get-ProvScheme -AdminAddress $_ |where {$_.machinecount -gt 0} 
+$Snapshot=split-path -path $result.MasterImageVM -leaf
+$MC=$result.IdentityPoolName
+
+  $report =@()
+  $report= for($i= 0; $i -lt $Result.count; $i++) {  
+    [PSCustomObject]@{
+    MC= $MC[$i]
+    Snapshot= $snapshot[$i]
+    }
+  }
+$report
+}
+
+
+<### --- Health check v1  --- ###>
+
 $ComputerName=""
 $computerName |out-file serverlistDDC.txt
 
@@ -7,3 +26,8 @@ $parameters = @{
 }
 
 Invoke-Command @parameters |select IdentityPoolName, MasterImageVM | export-csv MCSimage.csv
+
+
+
+
+
