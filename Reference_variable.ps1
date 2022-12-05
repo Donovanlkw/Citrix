ce618ed8ef65bdec8c93f3ab67c0aeaa7358282f
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-12-05 12:06:20 +0800

    Update Reference_variable.ps1

--- Reference_variable.ps1
+++ Reference_variable.ps1
@@ -1,32 +1,15 @@
-# ----- Variable ----#
+Get-Variable | Out-String 
+
+### --- Common Variable --- ###
+$Env: 
+$env:userdnsdomain 
+$Env:COMPUTERNAME 
+$FQNDServerName= [System.Net.Dns]::GetHostByName($env:computerName).HostName 
+
+### --- Variable --- ###
 $OBJ= New-Object -TypeName PSObject
 $OBJ | Add-Member -Name 'Name' -MemberType Noteproperty -Value 'Joe'
 $OBJ | Add-Member -Name 'Age' -MemberType Noteproperty -Value 32
-
-$array = @()
-$array += $obj
-
-
-#----- Array Appraoch#1 
-$updateFeatures = @(
-  'UpdateServices-WidDB',
-  'UpdateServices-Services',
-  'UpdateServices-RSAT',
-  'UpdateServices-API',
-  'UpdateServices-UI'
-)
-Write-Output $updateFeatures
-#Install-WindowsFeature -Name $updateFeatures
-
-#----- Array Appraoch#2
-$Directory= 'D:SRSReportKeys', 'F:MSSQL', 'F:MSSQLTempDB', 'F:MSSQLUserDB', 'G:MSSQL', 'G:MSSQLUserDBLOG', 'G:MSSQLTempDBLogs', 'G:MSSQLBackup'
-$Directory|foreach-object{
-New-Item $_ -Type Directory
-$NewAcl = Get-Acl -Path $_
-Set-Acl -Path $_ -AclObject $NewAcl
-}
-
-
 
 Write-Host
 Write-Output
@@ -34,12 +17,6 @@ Write-Error
 
 
 
-Get-Variable | Out-String 
-
-$Env: 
-$env:userdnsdomain 
-$Env:COMPUTERNAME 
-$FQNDServerName= [System.Net.Dns]::GetHostByName($env:computerName).HostName 
 
 #----- common command
 get-command  -Verb get -noun AD*Service*
@@ -48,7 +25,7 @@ Get-help *
 Get-history
 
 
-#----- Sample
+
 
 #----- Create a schedule Task in Admin. Tools.  
 $file = "c:\temp\$task.ps1"   
