4ade02f87c10aa48676f648925c8bca8caca6d86
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-12-09 11:23:06 +0800

    Update Healthchck.MCS.ps1

--- Healthchck.MCS.ps1
+++ Healthchck.MCS.ps1
@@ -17,6 +17,36 @@ $report |Format-Table
 }
 
 
+<### --- verificaiton  --- ###>
+
+ Get-ProvTask -MaxRecordCount 9999 | where{$_.Status -eq "Running"} | Stop-ProvTask
+ 
+###--- verification ---###
+Get-ProvTask |select Status, DateStarted,ProvisioningSchemeName, ProvisioningSchemeUid |sort-object DateStarted
+Get-ProvVM |select  VMName, BootedImage, LastBootTime  |Format-List
+
+
+### --- Validation --- ###
+Get-ProvTask -MaxRecordCount 999 |select Status, DateStarted,ProvisioningSchemeName, ProvisioningSchemeUid |sort-object DateStarted
+Get-ProvTask -MaxRecordCount 999 | where {$_.Status -eq “Running”}  |select Status, DateStarted,ProvisioningSchemeName, ProvisioningSchemeUid
+Get-ProvVM |select  VMName, BootedImage, LastBootTime  |Format-List
+
+
+
+
+### --- Validation --- ###
+Get-ProvTask |select Status, DateStarted,ProvisioningSchemeName, ProvisioningSchemeUid
+Get-ProvTask | where {$_.Status -eq “Running”}
+Get-ProvVM |select  VMName, BootedImage |Format-List
+
+
+Get-ProvTask | where {$_.Type -eq “DisusedImageCleanup” -and $_.Status -ne “Finished”}
+Get-ProvTask | where {$_.Type -eq “DisusedImageCleanup” -and $_.WorkflowStatus -eq “Terminated”} | Remove-ProvTask
+Get-ProvSchemeMasterVMImageHistory  –AdminAddress $DDC –ProvisioningSchemeUid $ProvSchemeGUID –SortBy “Date”
+
+ 
+ 
+
 <### --- Health check v1  --- ###>
 
 $ComputerName=""
