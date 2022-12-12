0d22a4293844e3086390dc84b833051ca6610b3a
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-12-12 11:42:23 +0800

    Update BAU.Software.ps1

--- BAU.Software.ps1
+++ BAU.Software.ps1
@@ -2,4 +2,4 @@
 
 $File="VM_NonProd"
 $ComputerName = Get-Content "$file.txt"
-Get-CimInstance -ComputerName $ComputerName -Class Win32_Product |select PSComputerName, Name, version|format-table -AutoSize| out-file "$file.sw.csv" -Append
+Get-CimInstance -ComputerName $ComputerName -Class Win32_Product |select PSComputerName, Name, version|format-table -AutoSize| out-file "output_$file.sw.csv" -Append
