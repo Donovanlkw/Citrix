465c8151424651c24c873b7db77d6586aa0714e8
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-12-12 11:02:54 +0800

    Update Healthcheck.Patching.ps1

--- Healthcheck.Patching.ps1
+++ Healthcheck.Patching.ps1
@@ -1,15 +1,15 @@
-### --- check all Patch level ---###
-$ComputerName = Get-Content 'ServerlistMaster.txt'
+### --- check all Patch level in last n days---###
+$ComputerName = @{}
+$ComputerName = $VM_core_JPE_JPW
 $AllResult = @()
-
 $StartTime=(Get-date).AddDays(-7)
+
 
 $ComputerName| Foreach-object {
 $PendingReboot=0
 $System = Get-CimInstance -ComputerName $_ -Class win32_operatingsystem
-$Hotfix = Get-hotfix  -computername $_  |where-object {$_.installedOn -gt $starttime}
-$CitrixService = Get-Service  -computername $_ -DisplayName Citrix* |where-object {$_.status -ne "Running"}
-
+$Hotfix = Get-hotfix  -ComputerName $_ |where-object {$_.installedOn -gt $starttime}
+$CitrixService = Get-Service  -ComputerName $_ -DisplayName Citrix* |where-object {$_.status -ne "Running"} |where-object {$_.StartType  -ne "disabled" }
 
 $reg=Invoke-Command -Computer $_ -ScriptBlock {
 ###--- https://batchpatch.com/explanation-of-get-pending-reboot-status-actions-in-batchpatch ---###
@@ -17,23 +17,10 @@ test-path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based
 test-path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -ErrorAction Ignore
 }
 
-if($reg -like "True"){
+
+if($reg -like "True"){ 
 $PendingReboot=1
 }
-
-#$reg=Invoke-Command -Computer $_ -ScriptBlock {
-#get-ItemProperty  -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing" -Name RebootPending  -ErrorAction Ignore
-#get-ItemProperty  -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing" -Name RebootPending  -ErrorAction Ignore 
-#get-ItemProperty  -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing" -Name PackagesPending -ErrorAction Ignore
-#get-ItemProperty  -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing" -Name RebootInProgress -ErrorAction Ignore
-#get-ItemProperty  -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name RebootRequired -ErrorAction Ignore
-#get-ItemProperty  -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name PostRebootReporting -ErrorAction Ignore
-#get-ItemProperty  -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services " -Name Pending -ErrorAction Ignore
-#}
-
-#if ($reg){
-#$PendingReboot=1
-#}
 
 $VMInfo = [pscustomobject]@{
 Name = $System.csname
