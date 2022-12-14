f2e4ad308f44c7d838804547d85f23fcaebcf666
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-12-14 09:48:27 +0800

    Update Healthcheck.XA.ps1

--- Healthcheck.XA.ps1
+++ Healthcheck.XA.ps1
@@ -1,8 +1,9 @@
+###--- check the Xenapp status ---###
+
 $parameters = @{
-  ComputerName = Get-Content VM_DDC_XA.txt
+  ComputerName = Get-Content "VM_DDC_XA.txt"
   ScriptBlock = {Get-Brokermachine -SessionSupport MultiSession -ProvisioningType MCS}
 }
 
-Invoke-Command @parameters |select CatalogName, DNSName, ImageOutOfDate, InMaintenanceMode, LoadIndex, SessionCount, RegistrationState |Format-table -AutoSize 
-
-###--- check the Xenapp status ---###
+$result = Invoke-Command @parameters
+$result |select CatalogName, DNSName, ImageOutOfDate, InMaintenanceMode, LoadIndex, SessionCount, RegistrationState |Sort-Object sessionCount, LoadIndex |Format-table -AutoSize 
