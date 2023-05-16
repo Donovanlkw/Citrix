60ad2b6c9c2dc10d8fd242733a51e5dff0607db2
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2023-05-16 15:18:56 +0800

    Update New.SiteConfig.ps1

--- New.SiteConfig.ps1
+++ New.SiteConfig.ps1
@@ -1,1 +1,11 @@
 New-AdminAdministrator -Name "MFCGD\APP-AZ Asia Citrix-Admin"
+
+### --- Citrix core setup ---###
+$ADGroup= "MFCGD\APP-AZ Asia Citrix-Admin"
+$Role="Read Only Administrator"
+
+New-AdminAdministrator -Name $ADGroup
+Add-AdminRight -Role $role  -All -Administrator $ADGroup
+
+$logdbconnection=Get-LogDBConnection
+set-logsite -state Enabled
