322649bde06e4fad750692617c7b9e0a21ad8822
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2023-05-17 16:03:18 +0800

    Create New.AGApp.ps1

---
+++ New.AGApp.ps1
@@ -0,0 +1,41 @@
+$Name= "NI-EAS-GA-S-STG"
+$DG="DG-"+ $Name
+$MC="MC-"+ $Name
+$AG="AG-"+ $Name
+$AGuser = "Domain\ACL"
+$Description = "General VDA"
+
+### --- New Application Group ---###
+New-BrokerApplicationGroup -name $AG -Description $Description
+Add-BrokerApplicationGroup $AG -DesktopGroup $DG
+Set-BrokerApplicationGroup $AG -UserFilterEnabled $true
+Add-BrokerUser $AGuser -ApplicationGroup $AG
+
+### --- New System  Application  ---###
+$app="notepad","mstsc","explorer","cmd"
+$app|foreach{
+$commandline ="%SystemRoot%\"+$_+".exe"
+$workingdirectory="%SystemDrive%\Windows"
+New-BrokerApplication -ApplicationType HostedOnDesktop -Name $_ -CommandLineExecutable $commandline -ApplicationGroup $AG  -WorkingDirectory $workingdirectory
+}
+
+### --- New Office  Application  ---###
+$app="WINWORD","EXCEL","POWERPNT","MSACCESS"
+
+C:\Program Files (x86)\Microsoft Office\Office16\.EXE
+$app|foreach{
+$commandline ="C:\Program Files (x86)\Microsoft Office\Office16\"+$_+".exe"
+$workingdirectory="C:\Program Files (x86)\Microsoft Office\Office16"
+New-BrokerApplication -ApplicationType HostedOnDesktop -Name $_ -CommandLineExecutable $commandline -ApplicationGroup $AG  -WorkingDirectory $workingdirectory
+}
+
+
+### --- New   Application  ---###
+$app="notepad","mstsc","explorer","cmd"
+$app|foreach{
+$commandline ="%SystemRoot%\"+$_+".exe"
+$workingdirectory="%SystemDrive%\Windows"
+New-BrokerApplication -ApplicationType HostedOnDesktop -Name $_ -CommandLineExecutable $commandline -ApplicationGroup $AG  -WorkingDirectory $workingdirectory
+}
+
+
