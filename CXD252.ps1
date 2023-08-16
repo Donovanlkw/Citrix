c63064ef360060d3b36f3c8da39be832ba89c6b0
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2023-08-16 10:42:53 +0100

    Update CXD252.ps1

--- CXD252.ps1
+++ CXD252.ps1
@@ -10,6 +10,10 @@ $VMName = "AUE-W10-001"
 $rgname = "Student52341Lab"
 
 Set-AzureRmVMCustomScriptExtension -ResourceGroupName $rgname -VMName $VMName -Name JoinDomainExtension -FileUri "https://cxd251impdata.blob.core.windows.net/vda/joindomain.ps1" -Run 'joindomain.ps1' -Argument 'azr.workspacelab.com AZR\ctxadmin Citrix@Pass!' -Location "East US 2"
-
-
 Remove-AzureRmVMExtension -ResourceGroupName $rgname -VMName $VMName -Name "JoinDomainExtension" -Force
+
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName $rgname -VMName $VMName -Name VDAInstallnExtension -FileUri "https://cxd251impdata.blob.core.windows.net/vda/InstallVDA_v1912.ps1" -Run 'InstallVDA_v1912.ps1' -Location "East US 2"
+Remove-AzureRmVMExtension -ResourceGroupName $rgname -VMName $VMName -Name "VDAInstallnExtension" -Force
+
+
+
