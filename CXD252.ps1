465001c2dcbc1cee2a667b6dd06357ec3e61f412
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2023-08-16 10:19:44 +0100

    Update CXD252.ps1

--- CXD252.ps1
+++ CXD252.ps1
@@ -1,3 +1,15 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Add-AzureRmAccount
 Save-AzureRmContext -Path c:\logon.json –Force
+
+
+$path = “C:\CXD-251\logon.json”
+Import-AzureRmContext -Path $path
+
+$VMName = "AUE-W10-001"
+$rgname = "Student52341Lab"
+
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName $rgname -VMName $VMName -Name JoinDomainExtension -FileUri "https://cxd251impdata.blob.core.windows.net/vda/joindomain.ps1" -Run 'joindomain.ps1' -Argument 'azr.workspacelab.com AZR\ctxadmin Citrix@Pass!' -Location "East US 2"
+
+
+Remove-AzureRmVMExtension -ResourceGroupName $rgname -VMName $VMName -Name "JoinDomainExtension" -Force
