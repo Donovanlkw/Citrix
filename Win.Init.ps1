c36cf0c06876c605a393fdd660e9bf7ecd3a2c7f
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-01-29 10:54:26 +0000

    Create Win.Init.ps1

---
+++ Win.Init.ps1
@@ -0,0 +1,10 @@
+$pshost = get-host
+$pswindow = $pshost.ui.rawui
+$newsize = $pswindow.buffersize
+$newsize.height = 3000
+$newsize.width = 300
+$pswindow.buffersize = $newsize
+$newsize = $pswindow.windowsize
+$newsize.height = 50
+$newsize.width = 300
+$pswindow.windowsize = $newsize
