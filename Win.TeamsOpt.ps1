c38fd083c125cbccd1f7c5e3c57f84cc5311e1d8
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-03-11 10:46:04 +0000

    Update Win.TeamsOpt.ps1

--- Win.TeamsOpt.ps1
+++ Win.TeamsOpt.ps1
@@ -2,10 +2,15 @@ $DG=""
 
 $today=Get-Date -f "yyyyMMdd"
 
+$Today=Get-Date -f "yyyyMMdd"
+
+$VDI=get-brokersession -MaxRecordCount 99999 -SessionState "active" -Protocol "HDX" -OSType "Windows 10"
+
+
 ###--- query all the DG VDI
 
-$VDI=get-brokersession -MaxRecordCount 99999 -SessionState "active" -Protocol "HDX"
-$ActiveVDI=$VDI
+$VDI=get-brokersession -MaxRecordCount 99999 -SessionState "active" -Protocol "HDX" -OSType "Windows 10"
+$ActiveVDI=$VDI |where Desktopgroup -eq $DG
 $ActiveVDI|select UserUPN, DNSName,  BrokeringTime, AgentVersion, ClientPlatform, ClientName, ClientVersion, ConnectedViaIP, SessionState |ft|out-file VDIsession_$DG"_"$today.txt
 $ActiveVDI.dnsname |out-file VDIlist_$DG"_"$today.txt
 
@@ -13,7 +18,7 @@ $ActiveVDI.dnsname |out-file VDIlist_$DG"_"$today.txt
 ###--- query active Computername by email .
 $userlist = Get-Content userlist.txt
 $DG=""
-$VDI=get-brokersession -MaxRecordCount 99999 -SessionState "active" -Protocol "HDX"
+$VDI=get-brokersession -MaxRecordCount 99999 -SessionState "active" -Protocol "HDX" -OSType "Windows 10"
 
 $ActiveVDI=$Userlist| Foreach-object {
 $VDI |where UserUPN -eq $_ 
@@ -23,9 +28,9 @@ $ActiveVDI|select UserUPN, DNSName,  BrokeringTime, AgentVersion, ClientPlatform
 $ActiveVDI.dnsname |sort |out-file VDIlist_$DG"_"$today.txt
 
 
-### --- MS Teams optimization validation webScoketAgent  --- ###
 
-$today=Get-Date -f "yyyyMMdd"
+
+### --- MS Teams optimization validation webScoketAgent  --- ###
 $ComputerName = Get-Content VDIlist_$DG"_"$today.txt
 
 $TeamsOptState=$ComputerName| Foreach-object {
@@ -43,12 +48,25 @@ $ComputerName = Get-Content TeamsOptFailure_$DG"_"$today.txt
 $ComputerName| Foreach-object {
 Invoke-Command -Computer $_ -ScriptBlock {
 Get-ItemProperty -Path "HKLM:SOFTWARE\WOW6432Node\Citrix\WebSocketService"
-Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Teams"
+#Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Teams"
 #Get-ItemProperty -Path "HKCU:SOFTWARE\Citrix\HDXMediaStream"
 Get-Process  -Name web*
 get-process -name web* |select PSComputerName,Name, productversion, starttime |ft
 get-process -name Teams* |select PSComputerName, Name, productversion, starttime, startinfo |ft
 get-process -name ms-Teams* |select startinfo |ft
-netstat -na |findstr 9001
+netstat -na |findstr 9002
+
 }
 }
+
+
+###--- query problem 's VDI issue. 
+
+$ComputerName = Get-Content TeamsOptFailure.txt
+
+$TeamsUsers = $ComputerName| Foreach-object {
+Get-Brokersession -dnsname $_ |select userupn, BrokeringTime, ClientName, ClientVersion, AgentVersion, SessionState
+}
+
+$TeamsUsers |ft 
+
