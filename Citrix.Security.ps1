bf813c72d4edf72fc7f61b132e2503acee09447b
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-04-29 15:41:39 +0100

    Update Citrix.Security.ps1

--- Citrix.Security.ps1
+++ Citrix.Security.ps1
@@ -1,2 +1,16 @@
 # https://www.citrix.com/about/legal/security-compliance/security-standards.html
 #Security and Compliance Information
+
+
+System Hardening Guidance for XenApp and XenDesktop
+
+Encyprtion between 
+
+SF 
+https://docs.citrix.com/en-us/storefront/current-release/install-standard/configure-https.html
+
+DDC (Connecting to NS/SF)
+https://support.citrix.com/article/CTX221671/how-to-enable-ssl-on-cloud-connectors-to-secure-xml-traffic 
+
+VDA (Connecting to NS/DDC)
+https://support.citrix.com/article/CTX220062/ssl-configuration-on-vda
