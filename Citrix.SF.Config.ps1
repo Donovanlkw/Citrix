7b5013a7f132a4401c1d39b20ce33c0d6c8bbf77
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-05-03 12:14:43 +0100

    Update Citrix.SF.Config.ps1

--- Citrix.SF.Config.ps1
+++ Citrix.SF.Config.ps1
@@ -61,5 +61,13 @@ Get-STFVersion
 #Wait-STFPublishServerGroupConfiguration
 #Wait-STFServerGroupJoin
 
+https://support.citrix.com/article/CTX206009
 
+https://developer-docs.citrix.com/projects/storefront-powershell-sdk/en/latest/Get-STFStoreService/
+
+https://dennisspan.com/translating-the-citrix-storefront-console-to-powershell/
+
+https://github.com/citrix/storefront-powershell-sdk-old/blob/master/docs/storefront-services-authentication-sdk.md
+
+https://www.mycugc.org/blogs/chris-jeucken/2021/03/02/unattended-storefront-install-and-config
 
