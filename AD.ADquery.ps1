eebe39b343179f34695142ddda1f1ab0665b4796
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-05-20 09:52:49 +0100

    Update AD.ADquery.ps1

--- AD.ADquery.ps1
+++ AD.ADquery.ps1
@@ -1,2 +1,11 @@
-##---  get the ACL of the computer object ---##
+### ---  get the ACL of the computer object --- ###
 Get-ADComputer  $computername | Get-ADPrincipalGroupMembership
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+### ---  Lookup computer object details via IP  computer object --- ###
+$ip=get-content ip.txt
+$dnshostname=($ip | Resolve-DnsName).namehost
+$ADhost=$dnshostname |foreach-object {Get-ADComputer -Properties * -filter 'dnshostname -like $_' }
+$ADhost |select dnshostname, description
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
