12c71ec2348e2f9af3502a569681ec55a10d750e
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-05-21 09:38:06 +0100

    Update and rename Citrix.DaaS.XA.Enq.ps1 to Citrix.DaaS.Query.ps1

--- Citrix.DaaS.XA.Enq.ps1
+++
@@ -1,9 +0,0 @@
-###--- check the Xenapp status ---###
-
-$parameters = @{
-  ComputerName = Get-Content "VM_DDC_XA.txt"
-  ScriptBlock = {Get-Brokermachine -SessionSupport MultiSession -ProvisioningType MCS}
-}
-
-$result = Invoke-Command @parameters
-$result |select CatalogName, DNSName, ImageOutOfDate, InMaintenanceMode, LoadIndex, SessionCount, RegistrationState |Sort-Object sessionCount, LoadIndex |Format-table -AutoSize 
