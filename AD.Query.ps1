398632e9f52b8d29614e8cc9e5f257086cf43f89
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-06-03 12:38:08 +0100

    Update and rename AD.ADquery.ps1 to AD.Query.ps1

--- AD.Query.ps1
+++ AD.Query.ps1
@@ -1,5 +1,26 @@
+### Searching --- ###
+
+### ---  A.D. User UPN and date related items--- ###
+$UPN="First.Last@Domain.com"
+$User=Get-ADUser -Filter{UserPrincipalName -eq $UPN} -Properties *
+$Userid=$user.SamAccountName
+$user=GET-ADuser $userid -Properties * 
+
+$User |select *date
+
+
+
+### --- search computer based on OU --- ###
+$SubOU = " OU=PROD"
+$SearchBase = $SubOU+",OU=Servers,DC=corp,DC=Domain,DC=net"
+
+$computername=Get-ADComputer -filter *  -SearchBase $SearchBase |select name
+
+
 ### ---  get the ACL of the computer object --- ###
 Get-ADComputer  $computername | Get-ADPrincipalGroupMembership
+
+
 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 ### ---  Lookup computer object details via IP  computer object --- ###
