4e230ddced3775392108c8560ef29dfdbcc8261e
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-06-04 14:24:52 +0100

    Update Citrix.validation.ps1

--- Citrix.validation.ps1
+++ Citrix.validation.ps1
@@ -1,16 +1,30 @@
+### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- 
+### ---  From End point to to Web --- ###
 $FQDN=
 "www.yahoo.com.hk"
 
 $IP=
 "76.223.84.192"
-
-
-$Result=$FQDN |ForEach-Object {
+$Result_EP2Web=$FQDN |ForEach-Object {
 Test-NetConnection $_ -port 443 
 }
-
-$Result | Select ComputerName, tcptestsucceeded, RemoteAddress
+$Result_EP2Web | Select ComputerName, tcptestsucceeded, RemoteAddress
 
 Compare-object -ReferenceObject $IP -DifferenceObject $Result.RemoteAddress.IPAddressToString -IncludeEqual
 
+### ---  From Performance Endpoint to to Web --- ###
+$performance=$FQDN |ForEach-Object {
+Measure-Command { Test-NetConnection $_ -port 443}
+}
+$Performance. totalseconds 
 
+
+### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- 
+
+### ---  From SF to DDC --- ###
+$Result_SF2CC= Invoke-Command -computer  $CtxSF -ScriptBlock {
+$Using:ctxccc |ForEach-Object {
+Test-netconnection $_ -port 80
+    }
+}
+$Result_SF2CC |select pscomputername, computername, remoteaddress, tcptestsucceeded, remoteport |ft
