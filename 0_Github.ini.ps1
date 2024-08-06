dbb898db9f3ca60d38b54a0ec14906384e94884d
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-08-06 12:30:11 +0100

    Update 0_Github.ini.ps1

--- 0_Github.ini.ps1
+++ 0_Github.ini.ps1
@@ -2,5 +2,11 @@
 ### --- Generate API Authentication Token
 https://github.com/settings/tokens/new
 
-invoke-restmethod -Uri https://api.github.com/users/donovanlkw
+$myToken = ""
+$authHeader = @{Authorization = "bearer $My_token"}
+$URL = "https://api.github.com/users/donovanlkw"
+
+
+invoke-restmethod -Uri $URL
+invoke-restmethod -Uri $URL -hteaders $authHeader
 
