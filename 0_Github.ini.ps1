7d50f6c717f41fbf9809a61c0d597a4af79961d8
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-08-06 15:35:16 +0100

    Update 0_Github.ini.ps1

--- 0_Github.ini.ps1
+++ 0_Github.ini.ps1
@@ -10,3 +10,16 @@ $URL = "https://api.github.com/users/donovanlkw"
 invoke-restmethod -Uri $URL
 invoke-restmethod -Uri $URL -hteaders $authHeader
 
+
+https://github.com/microsoft/powershellforgithub.
+$module_Name = "PowershellforGitHub"
+
+
+get-command -module powershellforgithub
+get-command -module powershellforgithub | out-gridview
+
+set-GitHubAuthentication -sessiononly
+
+$token = 
+
+get-
