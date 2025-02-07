bb922755e253287a6e81465f75979f1f62e7a0ce
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-02-07 17:19:33 +0000

    Update DaaS.New.SF.ps1

--- DaaS.New.SF.ps1
+++ DaaS.New.SF.ps1
@@ -1,4 +1,4 @@
-##### Definition #####
+### Definition ###
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2088631" -OutFile "C:\dotnet48.exe"
 
 Install-WindowsFeature -name  web-server
@@ -12,6 +12,57 @@ Start-Process -FilePath "C:\dotnet48.exe" -ArgumentList "/quiet /norestart" -Wai
 Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" -Name Release
 Restart-Computer -Force
 
+
+#https://docs.citrix.com/en-us/storefront/2203-ltsr/sdk-overview.html
+#https://docs.citrix.com/en-us/storefront/current-release/install-standard/create-new-deployment.html
+### Configure site ###
+# Add-STFDeployment -SiteID 1 -HostBaseURL "https://apps.example.com"
+
+
+Import-Module Citrix.StoreFront
+Import-Module Citrix.StoreFront.Stores
+Import-Module Citrix.StoreFront.Authentication
+Import-Module Citrix.StoreFront.WebReceiver
+  
+
+ Param(
+     [Parameter(Mandatory=$true)]
+     [Uri]$HostbaseUrl,
+     [long]$SiteId = 1,
+     [ValidateSet("XenDesktop","XenApp","AppController","VDIinaBox")]
+     [string]$Farmtype = "XenDesktop",
+     [Parameter(Mandatory=$true)]
+     [string[]]$FarmServers,
+     [string]$StoreVirtualPath = "/Citrix/Store",
+     [bool]$LoadbalanceServers = $false,
+     [int]$Port = 80,
+     [int]$SSLRelayPort = 443,
+     [ValidateSet("HTTP","HTTPS","SSL")]
+     [string]$TransportType = "HTTP"
+     )
+     \# Import StoreFront modules. Required for versions of PowerShell earlier than 3.0 that do not support autoloading
+     Import-Module Citrix.StoreFront
+     Import-Module Citrix.StoreFront.Stores
+     Import-Module Citrix.StoreFront.Authentication
+     Import-Module Citrix.StoreFront.WebReceiver
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#https://docs.citrix.com/en-us/storefront/current-release/install-standard/create-new-deployment.html
 
 
 https://support.citrix.com/article/CTX206009
