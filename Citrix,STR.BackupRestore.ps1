01f1b44b6fd17fdcc3ad0cda7a978c6538c16650
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-02-21 21:25:14 +0000

    Delete Citrix,STR.BackupRestore.ps1

--- Citrix,STR.BackupRestore.ps1
+++
@@ -1,7 +0,0 @@
-### --- Export a configuration. 
-Export-STFConfiguration -ZipFileName STFconfiguration.zip -targetfolder $env:tmp -NoEncryption
-
-### --- Import a configuration. 
-Clear-STFDeployment -Confirm $False
-Add-STFDeployment -siteID 1 
-Import-STFConfiguration -ConfigurationZip "$env:tmp\STFconfiguration.zip"
