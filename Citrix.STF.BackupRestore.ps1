b297e9f3f8ec8d56183911f62c9deae59c411505
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-02-21 11:02:01 +0000

    Update Citrix.STF.BackupRestore.ps1

--- Citrix.STF.BackupRestore.ps1
+++ Citrix.STF.BackupRestore.ps1
@@ -3,6 +3,17 @@
 
 Export-STFConfiguration -ZipFileName STFconfiguration.zip -targetfolder $env:tmp -NoEncryption
 
+
+Get-STFVersion
+Get-STFFeatureState
+Get-STFFeatureStateNames
+
+Get-STFDomainService
+Get-STFPackage
+Get-STFInstalledFeatures
+Get-STFPeerResolutionService
+Get-STFServiceMonitor
+
 ### --- Import a configuration. 
 Clear-STFDeployment -Confirm $False
 Add-STFDeployment -siteID 1 
