2cfee598af1edb2e8afccedda20f6ed03c050de4
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-02-24 10:28:35 +0000

    Update Win.Cert.ps1

--- Win.Cert.ps1
+++ Win.Cert.ps1
@@ -1,13 +1,10 @@
 ### --- Perform basic Certification installation.
-$computername|Foreach-object {
-    Invoke-Command -Computer $_ -ScriptBlock {
-        
-        ### --- Group policy update
-        GPupdate /force
 
         ### --- set the encryption standard
-        [Net.ServicePointManager]::Securityprotocol = [Net.ServicePointManager]::Securityprotocol::Ssl3 -bor  [Net.ServicePointManager]::Securityprotocol::Tls -bor   [Net.ServicePointManager]::Securityprotocol::Tls12
+        ### --- [Net.ServicePointManager]::Securityprotocol = [Net.ServicePointManager]::Securityprotocol::Ssl3 -bor  [Net.ServicePointManager]::Securityprotocol::Tls -bor   [Net.ServicePointManager]::Securityprotocol::Tls12
 
+
+        
         ### --- Install certification.
         ### --- https://support.citrix.com/s/article/CTX549325-how-to-install-digicert-g4-root-and-intermediate-certificates-in-cloud-connector?language=en_US 
         Invoke-WebRequest -Uri "https://cacerts.digicert.com/DigiCertTrustedRootG4.crt" -OutFile "C:\Temp\DigiCertTrustedRootG4.crt"
@@ -22,7 +19,8 @@ $computername|Foreach-object {
             FilePath = 'C:\Temp\DigiCertTrustedG4CodeSigningRSA4096SHA3842021CA1.crt'
             CertStoreLocation = 'Cert:\LocalMachine\CA'
         }
+
+
+        
         Import-Certificate @paramsRoot
         Import-Certificate @paramsInterRoot        
-    }      
-}
