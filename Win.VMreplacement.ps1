a5abd3a57f7a723e62c756edf46f2eab3c391528
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-02-24 10:48:05 +0000

    Create Win.VMreplacement.ps1

---
+++ Win.VMreplacement.ps1
@@ -0,0 +1,19 @@
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+### --- update the new server OU and Description as the old one. 
+$oriVM = "oldVM"
+$computername= "newVM"
+
+### --- AD information
+$oriDescription = (Get-ADComputer -Identity $oriVM -Properties *).Description
+$oriOU = (Get-ADComputer -Identity $oriVM -Properties *).DistinguishedName
+
+$computername |Foreach-object {
+get-adcomputer $computername|  Move-ADObject -TargetPath $TargetOU 
+Set-ADComputer -Identity $_ -Description $Description
+}
+
+### --- Windows content
+$oriHost = get-content \\$oriVM\c$\Windows\system32\drivers\etc\hosts | Select-String -NotMatch "^#|^$"
+# the certification
+# the local gpo/ localsec.
+# schedule task.
