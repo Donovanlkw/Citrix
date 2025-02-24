28b76e6d19280858afa3cc658f5bb8bc6229c79b
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-02-24 11:47:38 +0000

    Update Win.VMreplacement.ps1

--- Win.VMreplacement.ps1
+++ Win.VMreplacement.ps1
@@ -3,17 +3,49 @@
 $oriVM = "oldVM"
 $computername= "newVM"
 
-### --- AD information
+### --- get the AD information
 $oriDescription = (Get-ADComputer -Identity $oriVM -Properties *).Description
 $oriOU = (Get-ADComputer -Identity $oriVM -Properties *).DistinguishedName
+### --- host table
+$oriHost = get-content \\$oriVM\c$\Windows\system32\drivers\etc\hosts | Select-String -NotMatch "^#|^$"
 
+
+### --- applied to new VM
 $computername |Foreach-object {
 get-adcomputer $computername|  Move-ADObject -TargetPath $TargetOU 
 Set-ADComputer -Identity $_ -Description $Description
+Add-Content \\$_\c$\Windows\system32\drivers\etc\hosts  $oriHost
 }
 
-### --- Windows content
-$oriHost = get-content \\$oriVM\c$\Windows\system32\drivers\etc\hosts | Select-String -NotMatch "^#|^$"
-# the certification
-# the local gpo/ localsec.
-# schedule task.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### --- localsec, localgpo
+$oriSecpol=secedit /export /cfg $env:tmp\orisecpol.inf
+$targetcpol=get-content (secedit /export /cfg $env:tmp\orisecpol.inf)
+
+### --- the certification
+(Get-childitem -path cert:\localmachine\My |sort NotBefore |select Subject )[-1]
+Get-childitem -path cert:\localmachine\Root  |sort NotBefore 
+Get-childitem -path cert:\localmachine\CA |sort NotBefore
+
