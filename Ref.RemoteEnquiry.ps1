1fd64de232d3847d0da4714fc0d581b24cbc58db
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-03-10 09:52:34 +0000

    Update Ref.RemoteEnquiry.ps1

--- Ref.RemoteEnquiry.ps1
+++ Ref.RemoteEnquiry.ps1
@@ -5,6 +5,11 @@ $computerName |out-file serverlist.txt
 
 $parameters = @{
   ComputerName = Get-Content serverlist.txt
-  ScriptBlock = {Get-BrokerMachine -OSType "Windows 10" -filter {IsAssigned -eq $true} -MaxRecordCount 10000|measure-object  }
+  ScriptBlock = {Get-BrokerMachine -OSType "Windows 10" -filter {IsAssigned -eq $true} -MaxRecordCount 10000|measure-object
+  #Pass-through variable
+  write-host $using:computername
+  
+  
+  }
 }
 Invoke-Command @parameters |export-csv "Uniq VDI users.txt"
