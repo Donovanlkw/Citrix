b070ab8a14893d0969b043bc7b481923f1486bd1
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-03-24 15:54:09 +0000

    Update Win.VMreplacement.ps1

--- Win.VMreplacement.ps1
+++ Win.VMreplacement.ps1
@@ -1,38 +1,25 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 ### --- update the new server OU and Description as the old one. 
 $oriVM = "oldVM"
-$newVM= "newVM"
+$newVM = "newVM"
 
 ### --- get the AD information
-$oriDescription = (Get-ADComputer -Identity $oriVM -Properties *).Description
+$newDescription = (Get-ADComputer -Identity $oriVM -Properties *).Description
 $oriOU = (Get-ADComputer -Identity $oriVM -Properties *).DistinguishedName
-### --- host table
-$oriHost = get-content \\$oriVM\c$\Windows\system32\drivers\etc\hosts | Select-String -NotMatch "^#|^$"
-
+$newOU = $oriOU.Substring($oriOU.indexof(",")+1)
+$newHost = get-content \\$oriVM\c$\Windows\system32\drivers\etc\hosts | Select-String -NotMatch "^#|^$"
 
 ### --- applied to new VM
-$computername |Foreach-object {
-get-adcomputer $computername|  Move-ADObject -TargetPath $TargetOU 
-Set-ADComputer -Identity $_ -Description $Description
-Add-Content \\$_\c$\Windows\system32\drivers\etc\hosts  $oriHost
+$newVM |Foreach-object {
+get-adcomputer $_|  Move-ADObject  -TargetPath $newOU 
+Set-ADComputer -Identity $_ -Description $newDescription
+Add-Content \\$_\c$\Windows\system32\drivers\etc\hosts  $newHost
 }
-
-
-
-
-
-
-
-
-### --- localsec, localgpo
-$oriSecpol=secedit /export /cfg $env:tmp\orisecpol.inf
-$targetcpol=get-content (secedit /export /cfg $env:tmp\orisecpol.inf)
 
 ### --- the certification
 (Get-childitem -path cert:\localmachine\My |sort NotBefore |select Subject )[-1]
 Get-childitem -path cert:\localmachine\Root  |sort NotBefore 
 Get-childitem -path cert:\localmachine\CA |sort NotBefore
-
 $invokeCommand = @{
     ScriptBlock = {
         ### ---  get all the certification , excluded exiry cert.
@@ -40,12 +27,14 @@ $invokeCommand = @{
         Get-ChildItem -Path cert:\localmachine  -Recurse |select * | where-object {$_.NotAfter -gt $today}
     }
 }
-
-
 $oricert=Invoke-Command -Computer $oriVM @invokeCommand
 $newcert=Invoke-Command -Computer $newVM @invokeCommand
-
 ### ---  compare the cert. find some cert are missing in new server, but excluded the host based cert. 
 compare $oricert  -ReferenceObject $newcert  -Property Subject, PSParentPath, Thumbprint |sort |where-object {($_.SideIndicator -eq '=>') -AND ($_.Subject -NotMatch "$oriVM")}  |select PSParentPath, Thumbprint, subject
 
+
+
+### --- localsec, localgpo
+$oriSecpol=secedit /export /cfg $env:tmp\orisecpol.inf
+$targetcpol=get-content (secedit /export /cfg $env:tmp\orisecpol.inf)
 
