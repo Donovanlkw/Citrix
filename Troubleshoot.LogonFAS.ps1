e74075718218f9afe5ed4b970bcd30a8c2848f5b
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-03-31 16:47:41 +0100

    Update Troubleshoot.Logon.ps1

--- Troubleshoot.LogonFAS.ps1
+++ Troubleshoot.LogonFAS.ps1
@@ -7,6 +7,7 @@
 ### --- https://support.citrix.com/s/article/CTX560789-citrix-fas-incorrect-username-and-password?language=en_US
 ### --- https://support.citrix.com/s/article/CTX217150-unable-to-login-using-the-fas-authentication-getting-stuck-on-please-wait-for-local-session-manager?language=en_US
 
+
 $userid=""
 $VDA=""
 $ComputerName =  ""
@@ -22,10 +23,6 @@ $EndTime
 ### --- All FAS related error/warning log.
 $VDA_FASErr=Get-WinEvent -computername $VDA -FilterHashtable @{LogName='Application';ProviderName='Citrix.Authentication.IdentityAssertion';StartTime=$StartTime;EndTime=$EndTime} |where-object{$_.level -ne 4}|Select-Object -First 10
 $VDA_FASErr
-
-### --- AD authentication required time --- ###
-$VDA_AuthErr=(Get-WinEvent -computername $VDA -FilterHashtable @{LogName='System';StartTime=$StartTime;EndTime=$EndTime; ID=9}).message
-$VDA_AuthErr
 
 
 ### --- collect all the FAS error/warning log.
@@ -77,10 +74,20 @@ $fasusercert|select UserPrincipalName, ExpiryDate, @{name='FASServer'; expressio
 
 
 
+### --- FAS AD authentication ID=9--- ###
+$VDA_AuthErr=(Get-WinEvent -computername $VDA -FilterHashtable @{LogName='System';StartTime=$StartTime;EndTime=$EndTime; ID=9}).message
+$VDA_AuthErr
 
-
-
-
+### ---  101 check the FAS policy --- ### 
+$VDA| Foreach-object {
+Invoke-Command -Computer $_ -ScriptBlock {
+Get-ItemProperty -Path "HKLM:SOFTWARE\Policies\Citrix\Authentication\UserCredentialService\Addresses"
+Get-ItemProperty -Path "HKLM:SOFTWARE\WOW6432Node\Policies\Citrix\Authentication\UserCredentialService\Addresses"
+#gci "C:\Windows\System32\config\systemprofile\AppData\LocalLow\Microsoft\CryptnetUrlCache\Content"
+#(tnc opcs01028.corp.troweprice.net -port 80) |select  *
+CERTUTIL -dump
+}
+}
 
 
 
