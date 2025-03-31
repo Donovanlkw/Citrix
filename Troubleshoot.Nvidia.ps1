5d359b2ad8091240f7acbcbc125373da2ce058df
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-03-31 16:52:23 +0100

    Create Troubleshoot.Nvidia.ps1

---
+++ Troubleshoot.Nvidia.ps1
@@ -0,0 +1,15 @@
+### --- https://enterprise-support.nvidia.com/s/article/GPU-Utilization-using-NVSMI
+
+### --- GPU UTILIZATION USING NVSMI
+nvidia-smi -q -d utilization
+Nvidia-smi encodersessions
+dxdiag
+$nvlog="c:\Users\Public\Documents\NvidiaLogging\Log.NVDisplay.Container.exe.log"
+$nvlog=c:\"Program Files"\"NVIDIA Corporation"\NVSMI\nvidia-smi.exe -q
+$nvlog | select-string "licen"
+
+
+### ---https://enterprise-support.nvidia.com/s/article/How-to-guide-Using-nvidia-smi-on-host-to-monitor-GPU-behavior-with-vGPU-and-outputting-to-csv-format
+./nvidia-smi --query-gpu=timestamp,name,pci.bus_id,driver_version,pstate,pcie.link.gen.max,pcie.link.gen.current,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 1 
+
+### --- https://webglsamples.org/aquarium/aquarium.html
