1b20e7fbe7bbab4eca8be44a3e8c1d8581b9b880
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-04-01 22:53:44 +0100

    Update Citrix.DaaS.Monitor.ps1

--- Citrix.DaaS.Monitor.ps1
+++ Citrix.DaaS.Monitor.ps1
@@ -81,3 +81,10 @@ $URL = "$api/Sessions?$filter=StartDate gt 2020-03-01 and LogonDuration gt 20000
 # https://developer-docs.citrix.com/en-us/monitor-service-odata-api/data-access-protocol
 # https://developer-docs.citrix.com/en-us/citrix-cloud/accessing-monitor-service-data-citrix-cloud-external/accessing-monitor-service-data-citrix-cloud-external.html
 
+
+
+#####   https://www.powershellgallery.com/packages/CTXCloudApi/0.0.9/Content/Public%5CGet-CTXAPI_MonitorData.ps1
+
+
+
+
