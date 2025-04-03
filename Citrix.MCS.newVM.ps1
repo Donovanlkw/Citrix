84575f581b9224dff57b7a9ed01714ee742d949a
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-04-03 12:56:05 +0100

    Update Citrix.MCS.newVM.ps1

--- Citrix.MCS.newVM.ps1
+++ Citrix.MCS.newVM.ps1
@@ -1,12 +1,42 @@
-$x=(Get-BrokerCatalog -SessionSupport SingleSession).name
-
+$region = "OM"
 $count= "1"
-$MC= "MC"
-Get-AcctIdentityPool -IdentityPoolName $MC
-Get-BrokerCatalog -name $MC
 
-Get-AcctIdentityPool -IdentityPoolName $MC
-Get-BrokerCatalog -name $MC
+### --- Getting MC list  ---  ### 
+$MCList=Get-Brokercatalog -name *Provisioning*
+$MC=$MClist |Where-Object {$_.name -like "*$region*" -and $_.name -notlike "*11*"} 
+$MC.name
+
+
+### --- Creating Active Directory Machine Accounts
+
+#Get-AcctIdentityPool -IdentityPoolName $MC |select IdentityPoolName,NamingScheme,NamingSchemeType, OU
+$IdentityPoolName = Get-AcctIdentityPool -IdentityPoolName $MC.name
+$newAccounts = New-AcctADAccount -IdentityPoolName $IdentityPoolName.IdentityPoolName -Count $count
+$newAccounts 
+
+### --- Creating Machine  ---  ### 
+
+$Prov=Get-provscheme |where-object {$_.IdentityPoolName -like $IdentityPoolName.IdentityPoolName}
+New-provVM -ADAccountName $newAccounts.SuccessfulAccounts -ProvisioningSchemeName  $Prov.ProvisioningSchemeName
+
+New-brokermachine -CatalogUid n -MachineName 
+
+
+
+$newAccounts.SuccessfulAccounts | ForEach-Object { 
+  if( ! (New-BrokerMachine -MachineName $_.ADAccountSid -CatalogUid (Get-BrokerCatalog -Name $MC).Uid )) {
+    "Do some error handling here" 
+    } 
+
+
+
+
+
+
+
+
+$newProvVMtask = New-ProvVM -ADAccountName
+
 
 
 $newAccounts = New-AcctADAccount -IdentityPoolName $MC -Count $count
@@ -18,4 +48,15 @@ $taskDetails.CreatedVirtualMachines | ForEach-Object {
     "Do some error handling here" 
     } 
 }
+
+
+
+
+
+
+Get-Brokermachine -CatalogName $MC -MaxRecordCount 50 |select dnsname
+Get-Brokermachine -CatalogName $MC -MaxRecordCount 50 |New-BrokerHostingPowerAction -Action "TurnOn"
+Timeout 600
+get-brokermachine -Filter "((RegistrationState -eq `"unregistered`") -and (PowerState -eq `"On`") -and(SessionSupport -eq `"SingleSession`"))" -MaxRecordCount 500 | New-BrokerHostingPowerAction -Action "reset"
+
 
