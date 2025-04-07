16002fca57ce5c2bed769f1c89260f4517cc7528
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-04-07 13:01:12 +0100

    Update Citrix.MCS.newVM.ps1

--- Citrix.MCS.newVM.ps1
+++ Citrix.MCS.newVM.ps1
@@ -8,46 +8,27 @@ $MC=$MClist |Where-Object {$_.name -like "*$region*" -and $_.name -notlike "*11*
 $MC.name
 
 ### --- Creating Active Directory Machine Accounts
-
 #Get-AcctIdentityPool -IdentityPoolName $MC |select IdentityPoolName,NamingScheme,NamingSchemeType, OU
 $IdentityPoolName = Get-AcctIdentityPool -IdentityPoolName $MC.name
 $newAccounts = New-AcctADAccount -IdentityPoolName $IdentityPoolName.IdentityPoolName -Count $count  -ADUserName  $myCredential.Username -ADPassword $myCredential.Password
 #$newAccounts = New-AcctADAccount -IdentityPoolName $IdentityPoolName.IdentityPoolName -Count $count
 $newAccounts.SuccessfulAccounts
 
-
-
 ### --- Creating Machine  ---  ### 
 $Prov=Get-provscheme |where-object {$_.IdentityPoolName -like $IdentityPoolName.IdentityPoolName}
 New-provVM -ADAccountName $newAccounts.SuccessfulAccounts -ProvisioningSchemeName  $Prov.ProvisioningSchemeName
 
 
-
-New-brokermachine -CatalogUid n -MachineName xxx
+### --- Add VM to MC  ---  ### 
 $newAccounts.SuccessfulAccounts | ForEach-Object { 
-  if( ! (New-BrokerMachine -MachineName $_.ADAccountSid -CatalogUid (Get-BrokerCatalog -Name $MC).Uid )) {
+  if( ! (New-BrokerMachine -MachineName $_.ADAccountSid -CatalogUid (Get-BrokerCatalog -Name $MC.name).Uid )) {
     "Do some error handling here" 
-} 
-
-
-
-
-$newProvVMtask = New-ProvVM -ADAccountName
-
-$newAccounts = New-AcctADAccount -IdentityPoolName $MC -Count $count
-$newProvVMtask = New-ProvVM -ADAccountName
-$newAccounts.SuccessfulAccounts -ProvisioningSchemeName $MC -RunAsynchronously
-$taskDetails = Get-provTask -taskid $newProvVMtask.Guid
-$taskDetails.CreatedVirtualMachines | ForEach-Object { 
-  if( ! (New-BrokerMachine -MachineName $_.ADAccountSid -CatalogUid (Get-BrokerCatalog -Name $MC).Uid )) {
-    "Do some error handling here" 
-    } 
+  } 
 }
 
-Get-Brokermachine -CatalogName $MC -MaxRecordCount 50 |select dnsname
-Get-Brokermachine -CatalogName $MC -MaxRecordCount 50 |New-BrokerHostingPowerAction -Action "TurnOn"
-Timeout 600
-get-brokermachine -Filter "((RegistrationState -eq `"unregistered`") -and (PowerState -eq `"On`") -and(SessionSupport -eq `"SingleSession`"))" -MaxRecordCount 500 | New-BrokerHostingPowerAction -Action "reset"
-
+### --- Power On VM ---  ### 
+Get-Brokermachine -CatalogName $MC.name  -MaxRecordCount 10 |New-BrokerHostingPowerAction -Action "TurnOn"
+#Get-Brokermachine -CatalogName $MC.name  -MaxRecordCount 10 -Filter "((RegistrationState -eq `"unregistered`") -and (PowerState -eq `"On`") -and(SessionSupport -eq `"SingleSession`"))" |New-BrokerHostingPowerAction -Action "reset"
+Get-Brokermachine -CatalogName $MC.name |select dnsname
 
 ### --- https://support.citrix.com/s/article/CTX550420-how-to-add-machine-to-existing-machine-catalog-and-delivery-group-using-powershell?language=en_US
